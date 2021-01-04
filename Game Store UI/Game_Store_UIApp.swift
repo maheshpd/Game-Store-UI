8f5742e045042795ab169541435847b341c28195
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-04 11:23:27 +0530

    Initial Commit

---
+++ Game Store UI/Game_Store_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Store_UIApp.swift
+//  Game Store UI
+//
+//  Created by Mahesh Prasad on 04/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_Store_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
